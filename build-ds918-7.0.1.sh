e33711389d45bcd1639e1d150c5d805c144ef54c
郭乾有 <guoqianyou@163.com>
2021-12-11 18:20:34 +0800

    aaa

--- build-ds918-7.0.1.sh
+++ build-ds918-7.0.1.sh
@@ -28,8 +28,6 @@ cd ..
 cd redpill-load
 cp ${root}/user_config.DS918+.json ./user_config.json
 ./ext-manager.sh add https://raw.githubusercontent.com/jumkey/redpill-load/develop/redpill-acpid/rpext-index.json
-./ext-manager.sh add https://raw.githubusercontent.com/RedPill-TTG/redpill-sas-activator/master/rpext-index.json
-./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/igb/rpext-index.json
 ./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/mpt2sas/rpext-index.json
 ./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/master/mpt3sas/rpext-index.json
 sudo ./build-loader.sh 'DS918+' '7.0.1-42218'
